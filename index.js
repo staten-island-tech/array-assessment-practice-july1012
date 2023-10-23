180181da6f30c287fdad0bbabcaad2386ab91b09
july1012 <113192987+july1012@users.noreply.github.com>
2023-10-22 22:49:51 -0400

    efwq

--- index.js
+++ index.js
@@ -88,15 +88,78 @@ const titles = [
 //"--- wrote --- in ---"
 
 //Sort books from oldest to most recent
-
+const age = books.sort((a, b) => a.publishDate - b.publishDate);
+console.log(age);
 //sort books alphabetically
-
+const alph = books.sort((a, b) => {
+  const nameA = a.name.toUpperCase(); // ignore upper and lowercase
+  const nameB = b.name.toUpperCase(); // ignore upper and lowercase
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+});
+console.log(alph);
 //Find who wrote War and Peace
-
+books.forEach((book) => {
+  if (book.name == "War and Peace") {
+    console.log(book.authorFirst, book.authorLast);
+  }
+});
 //how many books were written before 1900?
-
+var boo = 0;
+books.forEach((book) => {
+  if (book.publishDate < 1900) {
+    boo++;
+  }
+});
+console.log(boo);
 //was there at least one book published within the last 100 years?
-
+var bo = false;
+books.forEach((book) => {
+  if (book.publishDate >= 1923) {
+    bo = true;
+  }
+});
+if (bo == true) {
+  console.log("yes");
+} else if (bo == false) {
+  console.log("no");
+}
 //was every book published within the last 100 years?
-
+var b = true;
+books.forEach((book) => {
+  if (book.publishDate < 1923) {
+    bo = false;
+  }
+});
+if (bo == false) {
+  console.log("no");
+} else if (b == true) {
+  console.log("yes");
+}
 //print a list of books that "includes" the genre historical
+
+//using push
+var hist = [];
+books.forEach((book) => {
+  if (book.genre.includes("historical")) {
+    hist.push(book);
+  }
+});
+
+//how to use map to only show certain properties/add properties
+console.log(hist);
+const hist2 = books.map((book, index) => ({
+  name: book.name,
+  price: 100,
+  id: index + 1,
+}));
+
+console.log(hist2);
+
+//filter
+const hist3 = books.filter((book) => book.genre.includes("historical"));
+console.log(hist3);
